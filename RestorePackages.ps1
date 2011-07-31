7be4d5d9c50c4d1083a7d98bf757389a55257f99
Graeme Foster <graemef@gmail.com>
2011-07-31 13:19:57 +0100

    Added NuGet.exe to avoid dependency on having it installed.

--- RestorePackages.ps1
+++ RestorePackages.ps1
@@ -2,5 +2,5 @@
 $packageConfigs = Get-ChildItem . -Recurse | where{$_.Name -eq "packages.config"}
 foreach($packageConfig in $packageConfigs){
 	Write-Host "Restoring" $packageConfig.FullName 
-	nuget i $packageConfig.FullName -o Packages
+	.\Tools\nuget.exe i $packageConfig.FullName -o Packages
 }
